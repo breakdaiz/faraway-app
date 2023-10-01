cb1130ef93b97efac9cf6f0a0515deea3895757a
bernard rayoso <rayosobernard@gmail.com>
2023-10-01 20:45:44 +0800

    Update fallback items

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 
 const Stats = ({ items }) => {
+  if (!items.length)
+    return (
+      <p className='stats'>
+        <em>Start adding some items to your packing list! 🚀</em>
+      </p>
+    );
+
   const numItems = items.length;
   const numPacked = items.filter(item => item.packed).length;
   const percentage = Math.round((numPacked / numItems) * 100);
 
   return (
     <footer className='stats'>
-      <em>
-        👜 You have {numItems} items on your list, and you already packed{" "}
-        {numPacked} ({percentage}
-        %)
-      </em>
+      {percentage === 100 ? (
+        <em>You got everything! Ready to go ✈️</em>
+      ) : (
+        <em>
+          👜 You have {numItems} items on your list, and you already packed{" "}
+          {numPacked} ({percentage}
+          %)
+        </em>
+      )}
     </footer>
   );
 };
