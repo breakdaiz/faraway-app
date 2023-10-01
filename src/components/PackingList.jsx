5caf06a5e81e7aef1f69236304361447691e9cbc
bernard rayoso <rayosobernard@gmail.com>
2023-10-01 21:06:45 +0800

    create sortin feature

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PackingList = ({ items, handleDeleteItems, handleToggleItem }) => {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
+  const handleSortBy = e => {
+    const selectedOption = e.target.value;
+    setSortBy(selectedOption);
+  };
+
   return (
     <div className='list'>
       <ul>
-        {items.map(item => (
+        {sortedItems.map(item => (
           <li key={item.id}>
             <input
               type='checkbox'
@@ -18,6 +36,18 @@ const PackingList = ({ items, handleDeleteItems, handleToggleItem }) => {
           </li>
         ))}
       </ul>
+      <div className='actions'>
+        <select
+          onChange={e => {
+            handleSortBy(e);
+          }}
+          value={sortBy}
+        >
+          <option value='input'> Sort by input order</option>
+          <option value='description'> Sort by description</option>
+          <option value='packed'> Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 };
